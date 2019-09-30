e0f2624010dafa67915d537d81c1432f7b24e596
Pinky Rajendrasinh Solanki <Psolanki6312@conestogac.on.ca>
2019-09-30 12:00:16 -0400

    create trianglesolver file and write basic code

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -10,7 +10,21 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
-            
+            string sside1, sside2, sside3;
+            int side1, side2, side3;
+
+            Console.Write("Enter side 1 for triangle: ");
+            sside1 = Console.ReadLine();
+            int.TryParse(sside1, out side1);
+
+            Console.Write("Enter side 2 for triangle: ");
+            sside2 = Console.ReadLine();
+            int.TryParse(sside2, out side2);
+
+            Console.Write("Enter side 3 for triangle: ");
+            sside3 = Console.ReadLine();
+            int.TryParse(sside3, out side3);
         }
     }
 }
+ 
